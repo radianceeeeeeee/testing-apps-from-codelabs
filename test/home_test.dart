4cae6cfa42d34fc49c4bf67b1dd8a3b08e93eae4
Radianceeeeeeee <97012089+radianceeeeeeee@users.noreply.github.com>
2023-03-25 20:52:43 +0800

    Initial commit

---
+++ test/home_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:testing_app/models/favorites.dart';
+import 'package:testing_app/screens/home.dart';
+
+Widget createHomeScreen() => ChangeNotifierProvider<Favorites>(
+      create: (context) => Favorites(),
+      child: const MaterialApp(
+        home: HomePage(),
+      ),
+    );
+
+void main() {
+  group('Home Page Widget Tests', () {
+    testWidgets('Testing if ListView shows up', (tester) async {
+      await tester.pumpWidget(createHomeScreen());
+      expect(find.byType(ListView), findsOneWidget);
+    });
+  });
+}
