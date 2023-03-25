fb7b70d190a60341b57f84bf3715ebe1515f36d7
Radianceeeeeeee <97012089+radianceeeeeeee@users.noreply.github.com>
2023-03-25 21:07:57 +0800

    Changed home_test.dart

--- test/home_test.dart
+++ test/home_test.dart
@@ -17,5 +17,18 @@ void main() {
       await tester.pumpWidget(createHomeScreen());
       expect(find.byType(ListView), findsOneWidget);
     });
+    // END OF NEW CONTENT
+
+    testWidgets('Testing Scrolling', (tester) async {
+      await tester.pumpWidget(createHomeScreen());
+      expect(find.text('Item 0'), findsOneWidget);
+      await tester.fling(
+        find.byType(ListView),
+        const Offset(0, -200),
+        3000,
+      );
+      await tester.pumpAndSettle();
+      expect(find.text('Item 0'), findsNothing);
+    });
   });
 }
